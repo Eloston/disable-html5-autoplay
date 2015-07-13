9751434fc9c59b68bad65a3e74bc5b9469461748
Eloston <Eloston@users.noreply.github.com>
2015-07-12 17:04:14 -0700

    Refactored code to deal with multiple frames in a page Updated README.md

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var g_tab_states = new Map();
+
 function chrome_api_callback() {
     if (chrome.runtime.lastError) {
         console.log("background.js: chrome.runtime.lastError: " + chrome.runtime.lastError.message);
@@ -29,17 +31,56 @@ function update_pageaction_visibility(tabid, visible) {
 };
 
 function handle_contentscript_message(message, sender, sendResponse) {
-    if (message.action == "update_pageaction_icon") {
-        update_pageaction_icon(sender.tab.id, message.suspended);
-    } else if (message.action == "update_pageaction_visibility") {
-        update_pageaction_visibility(sender.tab.id, message.visible);
+    if (message.action == "add_page") {
+        if (sender.frameId == 0) {
+            if (g_tab_states.has(sender.tab.id)) {
+                g_tab_states.delete(sender.tab.id);
+            };
+            g_tab_states.set(sender.tab.id, {
+                suspended: true,
+                frames_with_media: new Set()
+            });
+            update_pageaction_icon(sender.tab.id, g_tab_states.get(sender.tab.id).suspended);
+        } else {
+            if (!g_tab_states.has(sender.tab.id)) {
+                console.error("background.js: handle_contentscript_message - add_page: Tried to add frame, but tab was not registered in g_tab_states");
+                return false;
+            };
+        };
+        sendResponse(g_tab_states.get(sender.tab.id).suspended);
+    } else if (message.action == "page_has_media") {
+        if (!g_tab_states.has(sender.tab.id)) {
+            console.error("background.js: handle_contentscript_message - page_has_media: Tab is not registered: " + sender.tab.id.toString());
+            return false;
+        };
+        var already_visible = g_tab_states.get(sender.tab.id).frames_with_media.size > 0;
+        if (message.has_media == true) {
+            g_tab_states.get(sender.tab.id).frames_with_media.add(sender.frameId);
+        } else if (message.has_media == false) {
+            g_tab_states.get(sender.tab.id).frames_with_media.delete(sender.frameId);
+        } else {
+            console.error("background.js: handle_contentscript_message - page_has_media: Invalid value for message.has_media: " + JSON.stringify(message.has_media));
+            return false;
+        };
+        var now_visible = g_tab_states.get(sender.tab.id).frames_with_media.size > 0;
+        if (already_visible != now_visible) {
+            update_pageaction_visibility(sender.tab.id, now_visible);
+        };
     } else {
         console.error("background.js: handle_contentscript_message: Unknown message received: " + JSON.stringify(message));
     };
+    return false;
 };
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {action: "click_pageaction"});
+    if (!g_tab_states.has(tab.id)) {
+        console.error("background.js: pageAction onClicked event: Tab with id is not registered: " + tab.id.toString());
+        return false;
+    };
+    g_tab_states.get(tab.id).suspended = !g_tab_states.get(tab.id).suspended;
+    update_pageaction_icon(tab.id, g_tab_states.get(tab.id).suspended);
+    chrome.tabs.sendMessage(tab.id, {action: "update_suspended_state", suspended: g_tab_states.get(tab.id).suspended});
+    return false;
 });
 
 chrome.runtime.onMessage.addListener(handle_contentscript_message);
