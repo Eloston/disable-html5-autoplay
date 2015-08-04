f5a24363c1f28d123001be378ec8c22c96541b3f
Eloston <Eloston@users.noreply.github.com>
2015-08-03 22:47:19 -0700

    Enhanced Video.js code for players not using the autoplay parameter Fixed bug with autoplay attempt recording

--- background.js
+++ background.js
@@ -55,17 +55,15 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             set_dormant_icon(sender.tab.id);
         };
     } else if (message.action == "add_autoplay_attempt") {
-        if (tab_state.hasOwnProperty(message.element_type)) {
-            tab_state[message.element_type].attempts = tab_state[message.element_type].attempts + 1;
-            var total_attempts = 0;
-            for (element_type in tab_state) {
-                total_attempts = total_attempts + tab_state[element_type].attempts;
-            };
-            chrome.browserAction.setBadgeText({ tabId: sender.tab.id, text: total_attempts.toString() });
-        } else {
-            console.error("background.js: Tried to add autoplay attempt to a media type never encountered");
-            return false;
+        if (!tab_state.hasOwnProperty(message.element_type)) {
+            tab_state[message.element_type] = { count: 0, attempts: 0 };
         };
+        tab_state[message.element_type].attempts = tab_state[message.element_type].attempts + 1;
+        var total_attempts = 0;
+        for (element_type in tab_state) {
+            total_attempts = total_attempts + tab_state[element_type].attempts;
+        };
+        chrome.browserAction.setBadgeText({ tabId: sender.tab.id, text: total_attempts.toString() });
     } else {
         console.error("background.js: Unknown message.action received: " + JSON.stringify(message));
     };
