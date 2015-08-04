4e8fbeffc0fc55aead1024c92fccd964707221ba
Eloston <eloston@mail.com>
2015-08-03 18:42:37 -0700

    Fixed event listeners being removed in dynamically modified iframes Code cleanup

--- content_script.js
+++ content_script.js
@@ -1,29 +1,20 @@
-var frame_script = document.createElement("script");
-frame_script.src = chrome.extension.getURL("frame_script.js");
-frame_script.onload = function() {
-    this.parentNode.removeChild(this);
-};
-var old_document = document;
-var old_window = window;
-(document.head||document.documentElement).appendChild(frame_script);
-
 function forward_message(customEventInit) {
-    //console.log("received message");
     chrome.runtime.sendMessage(customEventInit.detail);
 };
 
 window.addEventListener("DisableHTML5AutoplayEvent_ToContentScript", forward_message);
 
-/*
-document.addEventListener("DOMContentLoaded", function(event) {
+document_observer = new MutationObserver(function(mutation_records) {
     window.addEventListener("DisableHTML5AutoplayEvent_ToContentScript", forward_message);
-    if (old_document !== document) {
-        console.log("document CHANGED!!!");
-    } else if (old_window !== window) {
-        console.log("window CHANGED!!!");
-    };
 });
 
-document.addEventListener("pagehide", function(event) { console.log("pagehide!!!"); });
-document.addEventListener("pageshow", function(event) { console.log("pageshow!!!"); });
-*/
+document_observer.observe(document, { childList: true });
+
+var frame_script = document.createElement("script");
+frame_script.src = chrome.extension.getURL("frame_script.js");
+frame_script.onload = function() {
+    if (this.hasOwnProperty("parentNode")) {
+        this.parentNode.removeChild(this);
+    };
+};
+(document.head||document.documentElement).appendChild(frame_script);
